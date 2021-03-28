f6af6989ddd55aff3efc38e1203bb3abfa43c179
vyacheslav gorb <slav2712.2@gmail.com>
2021-03-28 23:00:38 +0300

    Replace assertEquals with assertTrue in Validator test class

--- src/test/java/edu/gorb/array/validator/ArrayValidatorTest.java
+++ src/test/java/edu/gorb/array/validator/ArrayValidatorTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 public class ArrayValidatorTest {
 
@@ -16,20 +16,24 @@ public class ArrayValidatorTest {
     }
 
 
-    @Test(dataProvider = "array_lines")
-    public void testIsValidLine(String line, boolean expectedValue) {
+    @Test(dataProvider = "array_lines_valid")
+    public void testIsValidLine(String line) {
         boolean value = validator.isValidLine(line);
-        assertEquals(value, expectedValue);
+        assertTrue(value);
     }
 
-    @DataProvider(name = "array_lines")
+    @Test(dataProvider = "array_lines_invalid")
+    public void testIsNotValidLine(String line) {
+        boolean value = validator.isValidLine(line);
+        assertFalse(value);
+    }
+
+
+    @DataProvider(name = "array_lines_valid")
     public Object[][] createArrayLines() {
         return new Object[][]{
-                {"1, 2, 3, -4, 5, 6, 7", true},
-                {"1, -2, 3, 4, -5, 6, -7", true},
-                {"1, 2, 3, 4, 5, 6, 7sdffsdxv", false},
-                {"1, -02, 3, 4, 5, 6, 7", false},
-                {"--1, 2, 3, 4, 5, 6, 7", false}
+                {"1, 2, 3, -4, 5, 6, 7"},
+                {"1, -2, 3, 4, -5, 6, -7"}
         };
     }
 
